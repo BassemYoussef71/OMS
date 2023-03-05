e2d2b66de420fe950c2064ad9b0def4d68c1f733
BASSEM YOUSSEF <beaso_youssef@hotmail.com>
2023-03-05 19:53:46 +0200

    OFFICE5/4

---
+++ OMS.UseCases/Employees/AddEmployeeUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMS.UseCases.Employees.Interfaces;
+
+namespace OMS.UseCases.Employees
+{
+    public class AddEmployeeUseCase :IAddEmployeeUseCase
+    {
+
+    }
+}
